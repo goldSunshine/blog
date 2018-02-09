c506897403627fc4a5ab5f2a49506625b20659dc
goldSunshine <892697603@qq.com>
2018-02-08 20:15:28 -0500

    three

--- blog/views.py
+++ blog/views.py
@@ -15,5 +15,6 @@ def home(request):
     return render(request,'home.html',{'post_list':post_list})
 
 
-def detail():
-	return 
+def detail(request,id):
+	post = Article.objects.get(id = str(id))
+	return render(request,'post.html',{"post":post})
